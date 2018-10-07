1dd04663eb1c46c0e520d30aa35ab4a66877531e
cassioliveira <cassio@cassioliveira.com.br>
2018-10-07 03:19:29 -0300

    Cadastro de curso concluído

---
+++ src/main/java/br/edu/uepb/sigeris/repository/Cursos.java
@@ -0,0 +1,17 @@
+package br.edu.uepb.sigeris.repository;
+
+import br.edu.uepb.sigeris.model.Curso;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Cássio Oliveira <cassio@cassioliveira.com.br>
+ */
+public class Cursos extends Generic<Curso> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Cursos() {
+        super(Curso.class);
+    }
+}
