83629cf738d0f40e9551f4a234fb4a3cf8f99ae3
cassioliveira <cassio@cassioliveira.com.br>
2020-01-28 10:41:31 -0300

    Criação de arquivo para popular banco de dados para testes. Falta ativar a inicialização automática no persistence.xml.
    Atualização do plugin para mascara de dinheiro.

---
+++ src/main/resources/META-INF/sql/cadastros.sql
@@ -0,0 +1,7 @@
+INSERT INTO setor(id,nome,categoria) VALUES (1,'BIBLIOTECA','SERVIDORES');
+INSERT INTO setor(id,nome,categoria) VALUES (1,'CPD','SERVIDORES');
+INSERT INTO setor(id,nome,categoria) VALUES (1,'LIMPEZA','TERCEIRIZADOS');
+INSERT INTO setor(id,nome,categoria) VALUES (1,'ELETRICIDADE','TERCEIRIZADOS');
+INSERT INTO setor(id,nome,categoria) VALUES (1,'DIREÇÃO','SERVIDORES');
+
+INSERT INTO pessoa(id,nome,categoria) VALUES (1,'DIREÇÃO','SERVIDORES');
